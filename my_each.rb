8aa4c2d923463a7a292f719ddb1494930932ff39
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2018-09-06 15:04:28 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,7 +2,7 @@ def my_each(words) # put argument(s) here
   # code here
   if block_given?
     i = 0
-    while i <= words.length
+    while i < words.length
       yield(words[i])
       i += 1
     end
